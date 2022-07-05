b2831ce6635bdc2754975d56823a06dfb7742a2a
Trevor Bos <trevor.bos@outlook.com>
2022-07-04 20:38:16 -0400

    Added some initial constants, and some commented out stuff in the index to give me some guidance.

---
+++ index.js
@@ -0,0 +1,24 @@
+// Variables for node usage
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
+// This is where all of the variables are going to be placed.
+const createdHTML = require('./src/createHTML.js');
+
+// Variables for team members and team array
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+
+const workTeamArray = [];
+
+// Add the manager to the team, add to the array, move on to the employees
+
+
+// Add employees to the team, add to the array.
+
+
+// Create the function to generate the HTML page use FS (FileSystem)
+
+
+
