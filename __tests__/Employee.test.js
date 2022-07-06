a6af16e704d4df6242a11339f320db6e546b4640
Trevor Bos <trevor.bos@outlook.com>
2022-07-06 10:04:14 -0400

    Currently all tests for the employee fails. This is good.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,14 +1,48 @@
 //setting up the constructor
-const Employee = require('../lib/Employee');
+const Employee = require("../lib/Employee");
 
 //starting the tests
 
 //create the employee object
 
 test("This creates an employee object", () => {
-    const employee = new Employee ("Trevor", 117, "trevor.bos@outlook.com");
+  const employee = new Employee("Trevor", 117, "trevor.bos@outlook.com");
 
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+});
+
+//Grab the employee name
+
+test("This will grab the employee name", () => {
+  const employee = new Employee("Trevor", 117, "trevor.bos@outlook");
+
+  expect(employee.getEmployeeName()).toEqual(expect.any(String));
+});
+
+// Grab the employee ID number
+
+test("This will grab the employee ID number", () => {
+  const employee = new Employee("Trevor", 117, "trevor.bos@outlook");
+
+  expect(employee.getEmployeeID()).toEqual(expect.any(Number));
+});
+
+// Grab the employee email
+
+test("This will grab the employee email", () => {
+  const employee = new Employee("Trevor", 117, "trevor.bos@outlook");
+
+  expect(employee.getEmployeeEmail()).toEqual(
+    expect.stringContaining(employee.email.toString())
+  );
+});
+
+// Grab the employee role
+
+test("This will grab the employee role", () => {
+    const employee = new Employee("Trevor", 117, "trevor.bos@outlook");
+
+    expect(employee.getEmployeeRole()).toEqual("Employee");
 });
